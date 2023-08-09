38a8a5fc816c22f969f0bf593c25320516f6c3f9
zhaoyang <zhaoyang@lyky.cn>
2023-08-09 17:45:18 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,10 @@ Page({
     unitData: unitData,
 
     attackWeekendTitle: '',
-    attackWeekendCountdown: ''
+    attackWeekendCountdown: '',
+
+    teamLeagueTitle: '',
+    teamLeagueCountdown: ''
   },
   onLoad() {
     this.computePartyCountdownFn()
@@ -40,6 +43,7 @@ Page({
   computePartyCountdownFn() {
     // 活动倒计时
     this.getAttackWeekendFn()
+    this.getTeamLeagueFn()
   },
   getAttackWeekendFn() {
     let attackWeekendTitle = '突袭周末开始'
@@ -87,6 +91,109 @@ Page({
     this.setData({
       attackWeekendTitle,
       attackWeekendCountdown
+    })
+  },
+  getTeamLeagueFn() {
+    function getCurrentMonthDate() {
+      // 获取当前月有多少天
+      let date = new Date();
+      let year = date.getFullYear();
+      let month = date.getMonth() + 1;
+      let d = new Date(year, month, 0);
+      return d.getDate();
+    }
+
+    let teamLeagueTitle = '联赛开始'
+    let teamLeagueCountdown = ''
+
+    const nowDate = new Date()
+    let dateNumber = nowDate.getDate()
+    const hours = nowDate.getHours()
+    const minutes = nowDate.getMinutes()
+    const seconds = nowDate.getSeconds()
+
+    if ((dateNumber < 11 && dateNumber > 1) || (dateNumber === 11 && hours <= 16) || (dateNumber === 1 && hours >= 16)) {
+      teamLeagueTitle = '联赛结束'
+      let countdownDays = Math.abs(dateNumber - 10)
+      let countdownHours = 24 - hours
+      let countdownMinutes = 60 - minutes
+      let countdownSeconds = 60 - seconds
+      countdownHours = countdownHours + 16
+
+      if (seconds > 0) countdownMinutes = countdownMinutes - 1
+      if (minutes > 0) countdownHours = countdownHours - 1
+      if (countdownHours >= 24) {
+        countdownDays = countdownDays + 1
+        countdownHours = countdownHours - 24
+      }
+      teamLeagueCountdown = `${countdownDays}天${countdownHours}小时${countdownMinutes}分`
+    } else {
+      teamLeagueTitle = '联赛开始'
+      let countdownDays = getCurrentMonthDate() - dateNumber
+      let countdownHours = 24 - hours
+      let countdownMinutes = 60 - minutes
+      let countdownSeconds = 60 - seconds
+      countdownHours = countdownHours + 16
+
+      if (seconds > 0) countdownMinutes = countdownMinutes - 1
+      if (minutes > 0) countdownHours = countdownHours - 1
+      if (countdownHours >= 24) {
+        countdownDays = countdownDays + 1
+        countdownHours = countdownHours - 24
+      }
+      teamLeagueCountdown = `${countdownDays}天${countdownHours}小时${countdownMinutes}分`
+    }
+
+    this.setData({
+      teamLeagueTitle,
+      teamLeagueCountdown
+    })
+  },
+  getTeamRaceFn() {
+    let teamRaceTitle = '竞赛开始'
+    let teamRaceCountdown = ''
+
+    const nowDate = new Date()
+    let dateNumber = nowDate.getDate()
+    const hours = nowDate.getHours()
+    const minutes = nowDate.getMinutes()
+    const seconds = nowDate.getSeconds()
+
+    if (dateNumber > 22 || (dateNumber === 22 && hours >= 16)) {
+      teamRaceTitle = '竞赛结束'
+      let countdownDays = Math.abs(dateNumber - 7)
+      let countdownHours = 24 - hours
+      let countdownMinutes = 60 - minutes
+      let countdownSeconds = 60 - seconds
+      countdownHours = countdownHours + 16
+
+      if (seconds > 0) countdownMinutes = countdownMinutes - 1
+      if (minutes > 0) countdownHours = countdownHours - 1
+      if (countdownHours >= 24) {
+        countdownDays = countdownDays + 1
+        countdownHours = countdownHours - 24
+      }
+      teamRaceCountdown = `${countdownDays}天${countdownHours}小时${countdownMinutes}分`
+    } else {
+      teamRaceTitle = '竞赛开始'
+      let countdownDays = 4 - dateNumber
+      let countdownHours = 24 - hours
+      let countdownMinutes = 60 - minutes
+      let countdownSeconds = 60 - seconds
+      countdownHours = countdownHours + 16
+
+      if (seconds > 0) countdownMinutes = countdownMinutes - 1
+      if (minutes > 0) countdownHours = countdownHours - 1
+      if (countdownHours >= 24) {
+        countdownDays = countdownDays + 1
+        countdownHours = countdownHours - 24
+      }
+      teamRaceCountdown = `${countdownDays}天${countdownHours}小时${countdownMinutes}分`
+    }
+
+    this.setData({
+      teamRaceTitle,
+      teamRaceCountdown
     })
   }
 })
